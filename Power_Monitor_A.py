3761302092cfdd4b558a45d8e0ae104592871df5
j13tw <j13tw@yahoo.com.tw>
2018-11-06 15:08:17 +0800

    add check arduino data

--- Power_Monitor_A.py
+++ Power_Monitor_A.py
@@ -6,8 +6,6 @@ import json
 import datetime
 from serial import SerialException
 import paho.mqtt.client as mqtt
-# usbid version only for 1.0.3
-from usbid.device import usb_roots
 
 # MQTT setup data
 MQTT_SERVER = "10.20.0.19"
@@ -16,35 +14,26 @@ MQTT_TOPIC = "cabinet_A"
 
 power = serial.Serial()
 
-def arduino_connect():
-	global power
-	power_tty = ""
-	for usb_id in range(1, 10):
-		try:
-			usb_info = usb_roots()[1][1][usb_id]
-		except:
-			usb_info = " "
-		if (usb_info != " "):
-			if (usb_info.idVendor == "067b" and usb_info.idProduct == "2303"):
-				power_tty = usb_info.tty
-				print("power_tty -->" + power_tty)
-			else:
-				print("arduino plugin error")
-		if (power_tty != ""):
-			break
-	try:
-		power = serial.Serial('/dev/' + power_tty, 9600, timeout=1)
-		time.sleep(2.5)
-	except:
-		power = serial.Serial()
-		time.sleep(1)
-
 while(1):
 	if(power.is_open):
+		send_status = 0
 		response = power.readline()
 		response = response.decode('ascii')
 		print(response)
-		if (response != ""):
+		try:
+			data = json.loads(str(response))
+			IN_V110_A = data['IN_V110_A']
+			IN_V110_B = data['IN_V110_B']
+			OUT_V110_A = data['OUT_V110_A']
+			OUT_V110_B = data['OUT_V110_B']
+			OUT_V110_C = data['OUT_V110_C']
+			OUT_V110_D = data['OUT_V110_D']
+			OUT_V110_E = data['OUT_V110_E']
+#			print(Temperature, type(Temperature), Humidity, type(Humidity))
+			send_status = 1
+		except:
+			send_status = 0
+		if (send_status == 1):
 			print('------------------------------------------------------')
 			try:
 				# MQTT connection
@@ -57,9 +46,8 @@ while(1):
 				print('MQTT To Server Error !')
 			print('------------------------------------------------------')
 	else:
-#		arduino_connect()
 		try:
-			power = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+			power = serial.Serial('COM30', 9600, timeout=1)
 			time.sleep(2.5)
 		except:
 			power = serial.Serial()
